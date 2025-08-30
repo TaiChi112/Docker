dfbcaf5d89e5f6f2b874083272eb027179a35f1a
TaiChi112 <anothai.0978452316@gmail.com>
2025-08-30 17:10:03 +0700

    feat: add main.cpp with initial output messages for container workspace

---
+++ Operating_System/workspace_local/src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+	cout<<"Dev in container workspace mapping in local workspace"<<endl;
+	cout<<"I try to edit local workspace mapping in container workspace"<<endl;
+	return 0;
+}
